8d137282b06eea9cec1a501fb57765728538bd3c
jorge <jromari069@g.educaand.es>
2024-02-27 14:09:32 +0100

    proyectoReact_Actualizado_27/02/2024_002

---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+
+//Actualización nueva 27/02/2024
+const Footer = () => {
+    return (  
+        <footer>
+        <p className="author">🪐 by 
+            <a href="https://github.com/dnlsilv" target="_blank" rel="noopener noreferrer">Daniel Silva</a> & 
+            <a href="https://github.com/jorge1RA/proyectoReact.git" target="_blank" rel="noopener noreferrer">Jorge Romero</a>
+        </p>
+        </footer>
+    
+    );
+}
+ 
+export default Footer;
---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+
+
+const Header = () => {
+    return ( 
+        <header className="hero">
+            <h2>Buscador de Emojis 🧐</h2>
+            <h3>Busca tu Emoji favorito...</h3>
+        </header>
+    );
+}
+ 
+export default Header;
